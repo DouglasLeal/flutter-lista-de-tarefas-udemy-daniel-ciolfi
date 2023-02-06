47a1b9fcae842dea630e6bad98fedd97388b0413
Douglas Leal <douglas.leal@live.com>
2023-02-05 23:21:25 -0300

    Criados AppBar, TextField e Button adicionar

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -12,6 +12,37 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: _appBar(),
+      body: _body(),
+    );
+  }
+
+  _appBar() {
+    return AppBar(
+      title: const Text("Tarefas"),
+      centerTitle: true,
+    );
+  }
+
+  _body() {
+    return Column(
+      children: [
+        Container(
+          margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+          child: Row(
+            children: [
+              const Expanded(
+                child: TextField(),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Icon(Icons.add),
+              ),
+            ],
+          ),
+        )
+      ],
+    );
   }
 }
